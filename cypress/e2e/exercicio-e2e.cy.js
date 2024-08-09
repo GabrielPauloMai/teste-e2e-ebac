dac1daeadc6f2c549a983063930031afe1f3a81f
Gabriel Mai <gabriel.mai@belluno.digital>
2024-08-09 15:45:15 -0300

    refactor: Update e2e test for ordering products in Ebac Shop

--- cypress/e2e/exercicio-e2e.cy.js
+++ cypress/e2e/exercicio-e2e.cy.js
@@ -1,13 +1,6 @@
 /// <reference types="cypress" />
 import ProdutosPage from "../support/page_objects/produtos.page";
 context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
-  /*  Como cliente 
-      Quero acessar a Loja EBAC 
-      Para fazer um pedido de 4 produtos 
-      Fazendo a escolha dos produtos
-      Adicionando ao carrinho
-      Preenchendo todas opções no checkout
-      E validando minha compra ao final */
   before(() => {
     cy.fixture('perfil').then(userProfile => {
       ProdutosPage.performLogin(userProfile)
@@ -15,17 +8,27 @@ context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
   });
 
   it('Deve fazer um pedido na loja Ebac Shop de ponta a ponta', () => {
+    const checkoutData = ProdutosPage.generateCheckoutData()
+    let productsAdded = []
     cy.visit('produtos')
     cy.fixture('products').then((productData) => {
       productData.products.forEach(item => {
         ProdutosPage.visitProductPage(item)
-        ProdutosPage.addToCart(item)
+        productsAdded.push(ProdutosPage.addToCart(item))
         cy.get('.woocommerce-message').should('contain', ProdutosPage.createAddToCartMessage(item))
       })
     })
     cy.visit('checkout')
-    ProdutosPage.fillCheckoutForm()
+    ProdutosPage.fillCheckoutForm(checkoutData)
     cy.get('.woocommerce-thankyou-order-received').should('contain', 'Obrigado. Seu pedido foi recebido.')
-    ProdutosPage.verifyOrderDetails()
+
+    cy.fixture('products').then((products) => {
+      products.products.forEach(product => {
+        productsAdded.push(product)
+      })
+    })
+
+    ProdutosPage.verifyOrderDetails(productsAdded)
+    ProdutosPage.verifyCheckoutData(checkoutData)
   });
 });
