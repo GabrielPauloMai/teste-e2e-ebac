58fc0414f0adeef4d81ec3219d6715c699404290
Gabriel Mai <gabriel.mai@belluno.digital>
2024-08-28 13:33:32 -0300

    feat: add new keys

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  projectId: 's8sxqz',
+  projectId: '9kk7ak',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
